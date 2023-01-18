8e6d0fca74140331f3c72782d0bf2e9aa1dc4cef
n9dmitry <121239886+n9dmitry@users.noreply.github.com>
2023-01-18 13:30:24 +0300

    v0.1

---
+++ products/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(max_length=1000)
+    image = models.ImageField(upload_to='products/', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
