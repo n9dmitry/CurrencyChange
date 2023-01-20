184ca8cb0dab276bf8bf4b2eed22d33c5eb57ac5
n9dmitry <121239886+n9dmitry@users.noreply.github.com>
2023-01-20 20:19:12 +0300

    v0.44

--- currencyconverter/settings.py
+++ currencyconverter/settings.py
@@ -64,8 +64,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-
-                'django.template.context_processors.media',
             ],
         },
     },
--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.http import HttpResponse
+import requests
+from .models import Product
 
 def store_view(request):
     return render(request, 'products/base.html')
@@ -9,3 +11,14 @@ def index(request):
     return render(request, 'products/index.html')
 
 
+api_key = '0b3a0600039b4a438508b98feb11b8a7'
+def get_currency_rate(api_key, currency_code):
+    url = f'https://openexchangerates.org/api/latest.json?app_id={api_key}'
+    response = requests.get(url)
+    data = response.json()
+    return data['rates'].get(currency_code)
+
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'products/index.html', {'products': products})
