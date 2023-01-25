96ed86726eb46df055b7733072ff6209a863ace2
n9dmitry <121239886+n9dmitry@users.noreply.github.com>
2023-01-25 14:26:00 +0300

    v0.53

--- products/urls.py
+++ products/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .views import product_list
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import actual_rates
 
 app_name = 'products'
 
 urlpatterns = [
 #    path('', views.index, name='products'),
     path('', product_list, name='product_list'),
+    path('actual_rates/', actual_rates, name='actual_rates'),
 ]
 
--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 import requests
 from .models import Product
 from .models import Currency
@@ -18,17 +18,25 @@ def product_list(request):
     return render(request, 'products/index.html', {'products': products})
 
 
-def update_currency(request):
-    if request.method == 'POST':
-        api_key = '0b3a0600039b4a438508b98feb11b8a7'
-        url = 'https://openexchangerates.org/api/latest.json?app_id=' + api_key
-        response = requests.get(url)
-        data = response.json()
-        rates = data['rates']
-        # сохраняем "рейтс" в базу
-        for name, rate in rates.items():
-            obj, created = Currency.objects.update_or_create(
-                name=name,
-                defaults={'rate': rate}
-                )
-        messages.success(request, 'Курсы валют обновлены!')
+# def update_currency(request):
+#     if request.method == 'POST':
+#         api_key = '0b3a0600039b4a438508b98feb11b8a7'
+#         url = 'https://openexchangerates.org/api/latest.json?app_id=' + api_key
+#         response = requests.get(url)
+#         data = response.json()
+#         rates = data['rates']
+#         # сохраняем "рейтс" в базу
+#         for name, rate in rates.items():
+#             obj, created = Currency.objects.update_or_create(
+#                 name=name,
+#                 defaults={'rate': rate}
+#                 )
+#         messages.success(request, 'Курсы валют обновлены!')
+
+def actual_rates(request):
+    api_key = '0b3a0600039b4a438508b98feb11b8a7'
+    url = 'https://openexchangerates.org/api/latest.json?app_id=' + api_key
+    response = requests.get(url)
+    api_data = response.json()
+    return JsonResponse(api_data)
+
