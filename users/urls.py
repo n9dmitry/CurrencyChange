64524219cff770586b272422e848bbd0fd1eea16
n9dmitry <121239886+n9dmitry@users.noreply.github.com>
2023-02-01 23:58:30 +0300

    v0.58

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
+from users.views import registration
 
 app_name = 'users'
+
+urlpatterns = [
+    path('registration/', registration, name='registration'),
+
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def registration(request):
+    context = {
+        'text': 'Регистрация'
+    }
+    return render(request, 'registration.html', context)
