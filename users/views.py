835be6ea7253434f7abd819768ae75542f9d5fb2
n9dmitry <121239886+n9dmitry@users.noreply.github.com>
2023-02-02 16:00:57 +0300

    v0.60

--- users/views.py
+++ users/views.py
@@ -1,21 +1,20 @@
-from django.contrib.auth import authenticate, login
-from django.shortcuts import render, redirect
-import django.contrib.auth.views as auth_views
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
 
 def login(request):
-    if request.method == 'POST':
-        # Запрашиваем имя пользователя и пароль из HTML-формы
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        # Проверяем правильность имени пользователя и пароля
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            # Аутентификация прошла успешно
-            login(request, user)
-            # Перенаправляем пользователя на главную страницу
-            return redirect('index')
-        else:
-            # Аутентификация не удалась
-            return redirect('login')
+    return render(request, 'users/login.html',)
+    # if request.method != 'POST':
+    #     # Запрашиваем имя пользователя и пароль из HTML-формы
+    #     username = request.POST.get('username')
+    #     password = request.POST.get('password')
+    #
+    #     # Проверяем правильность имени пользователя и пароля
+    #     user = authenticate(username=username, password=password)
+    #     if user is not None:
+    #         # Аутентификация прошла успешно
+    #         login(request, user)
+    #         # Перенаправляем пользователя на главную страницу
+    #         return redirect('index')
+    #     else:
+    #         # Аутентификация не удалась
+    #         return redirect('login')
