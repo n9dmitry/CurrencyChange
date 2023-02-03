9b8ee6073e3d0d6239f06a69f34a37050410ffbd
n9dmitry <121239886+n9dmitry@users.noreply.github.com>
2023-02-04 00:21:45 +0300

    v0.63

--- products/views.py
+++ products/views.py
@@ -14,25 +14,10 @@ def index(request):
     return render(request, 'products/index.html')
 
 
-def product_list(request):
+def product_list(request, currency=None):
     products = Product.objects.all()
     return render(request, 'products/index.html', {'products': products})
 
-
-# def update_currency(request):
-#     if request.method == 'POST':
-#         api_key = '0b3a0600039b4a438508b98feb11b8a7'
-#         url = 'https://openexchangerates.org/api/latest.json?app_id=' + api_key
-#         response = requests.get(url)
-#         data = response.json()
-#         rates = data['rates']
-#         # сохраняем "рейтс" в базу
-#         for name, rate in rates.items():
-#             obj, created = Currency.objects.update_or_create(
-#                 name=name,
-#                 defaults={'rate': rate}
-#                 )
-#         messages.success(request, 'Курсы валют обновлены!')
 
 def actual_rates(request):
     api_key = '0b3a0600039b4a438508b98feb11b8a7'
@@ -43,12 +28,13 @@ def actual_rates(request):
         Currency.objects.update_or_create(name=name, defaults={'rate': rate})
     return JsonResponse(api_data)
 
+
 def convert_currency(request, currency):
     print(f'{currency=}')
     products = Product.objects.all()
     rate = Currency.objects.get(name=currency).rate
     base_rate = Currency.objects.get(name='RUB').rate
     for product in products:
-        product.price = float(product.price) / float(base_rate) * float(rate)
-    return HttpResponse(' jj oijq')
+        product.price = round((float(product.price) / float(base_rate) * float(rate)), 2)
+    return render(request, 'products/index.html', context={'currency': currency, 'products': products})
 
