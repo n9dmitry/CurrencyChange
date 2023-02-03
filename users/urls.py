3fb07ffa71f4d0d2bc5bcdf3c3f61ba2bbcc0298
n9dmitry <121239886+n9dmitry@users.noreply.github.com>
2023-02-03 16:22:43 +0300

    v0.62

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from users.views import login
+from .views import register
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', login, name='login'),
+    path('registration/', register, name='registration'),
+
 ]
--- users/views.py
+++ users/views.py
@@ -1,19 +1,43 @@
+from urllib import request
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
 
 
-def login(request):
+def LoginUser(requrst):
     if request.method == 'GET':
         return render(request, 'users/login.html')
 
+    # if request.method == 'POST':
+    #     username = request.POST.get('username')
+    #     password = request.POST.get('password')
+    #     print(username)
+    #     user = authenticate(username=username, password=password)
+    #     if user is not None:
+    #         login(request, user)
+    #         return redirect('index')
+    #     else:
+    #         return redirect('login')
+
+# / вот выше нихуя не получается
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+
+def register(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        print(username)
-        user = authenticate(username=username, password=password)
-        if user is not None:
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
             login(request, user)
-            return redirect('index')
-        else:
-            return redirect('login')
+            return redirect('home')
+    else:
+        form = RegistrationForm()
+    return render(request, 'users/registration.html', {'form': form})
